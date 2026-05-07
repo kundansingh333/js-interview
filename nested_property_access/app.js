c8db0b3ba2bae994807af1691f5c76a326e94412
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-05-07 21:58:47 +0530

    optimal chaining

---
+++ nested_property_access/app.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+const user = {
+  name: "John Doe",
+};
+
+console.log(user.name); // Output: John Doe
+console.log(user?.name); // Output: John Doe
+
+console.log(user.address?.city); // Output: undefined (address is not defined in user object that's why it returns undefined instead of throwing an error)
+
+// truthy checking:
+
+console.log(user.address && user.address.city); // check if address exists and then access city property, if address is undefined it will return undefined instead of throwing an error
