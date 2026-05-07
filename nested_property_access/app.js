e168299ea9cff27ba6590dfc1908d99b8314727c
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-05-07 22:00:21 +0530

    some changes in the nested_properety_access

--- nested_property_access/app.js
+++ nested_property_access/app.js
@@ -11,3 +11,5 @@ console.log(user.address?.city); // Output: undefined (address is not defined in
 // truthy checking:
 
 console.log(user.address && user.address.city); // check if address exists and then access city property, if address is undefined it will return undefined instead of throwing an error
+
+console.log(user.address?.city); // using optional chaining operator to access city property, if address is undefined it will return undefined instead of throwing an error
